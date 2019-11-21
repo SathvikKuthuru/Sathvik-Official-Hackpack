376c59553d05d6cd46aea0b39c51f8f2c237d539
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2019-11-21 12:23:44 -0500

    Update Trie.java

--- Trie.java
+++ Trie.java
@@ -8,6 +8,9 @@ public class TrieTest {
 		root.insertWord("cool");
 		System.out.println(root.hasWord("cool"));
 		System.out.println(root.hasWord("oofs"));
+		System.out.println(root.hasWord("oof"));
+		root.deleteWord("oof");
+		System.out.println(root.hasWord("oof"));
 	}
 	
 	static class Trie {
@@ -33,10 +36,38 @@ public class TrieTest {
 			Trie curr = this;
 			for(int i = 0; i < in.length(); i++) {
 				int at = in.charAt(i) - 'a';
-				if(children[at] == null) children[at] = new Trie();
+				if(children[at] == null) return false;
 				curr = children[at];
 			}
 			return curr.wordCount > 0;
+		}
+		
+		void deleteWord(String in) {
+			deleteWord(in, -1);
+		}
+		
+		boolean deleteWord(String in, int depth) {
+			if(depth == in.length()-1) {
+				wordCount--;
+				if(wordCount == 0) {
+					for(Trie c : children) {
+						if(c != null) return false;
+					}
+					return true;
+				}
+				return false;
+			}
+			int next = in.charAt(depth+1) - 'a';
+			if(children[next] == null) {
+
+				return false;
+
+			}
+			if(children[next].deleteWord(in, depth+1)) {
+				children[next] = null;
+				return wordCount == 0;
+			}
+			return false;
 		}
 		
 	}
