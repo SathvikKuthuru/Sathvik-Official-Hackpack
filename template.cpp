0136bea91478cc7fa7a77d3934ce28ebebf5b43a
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2021-09-30 08:06:40 -0400

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<pd> vpd;
+
+
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    return 0;
+}
