8ac01868bdceeae2f1adf91f69b61318b398db89
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2024-04-23 01:51:14 -0400

    Create centroid_decomp.cpp

---
+++ centroid_decomp.cpp
@@ -0,0 +1,36 @@
+const int MAXN = (int) (2e5 + 10);
+
+vi adj[MAXN];
+int sub[MAXN];
+bool rem[MAXN];
+int n, q;
+
+void dfs_sub(int at, int p) {
+  sub[at] = 1;
+  for(int x : adj[at]) {
+    if(x == p || rem[x]) continue;
+    dfs_sub(x, at);
+    sub[at] += sub[x];
+  }
+}
+
+int dfs_cent(int at, int p, int cnt) {
+  for(int x : adj[at]) {
+    if(x == p || rem[x]) continue;
+    if(sub[at] * 2 > cnt) {
+      return dfs_cent(x, at, cnt);
+    }
+  }
+  return at;
+}
+
+void cent_decomp(int root) {
+  dfs_sub(root, -1);
+  int cent = dfs_cent(root, -1, sub[root]);
+  // Do Stuff
+  rem[cent] = true;
+  for(int x : adj[root]) {
+    if(rem[x]) continue;
+    cent_decomp(x);
+  }
+}
